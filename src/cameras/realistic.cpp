185353dc935caa344e05b7308a94116bc98fd4c6
JayHsieh <k3331863@gmail.com>
2015-11-23 21:35:20 +0800

    lab 2012

--- src/cameras/realistic.cpp
+++ src/cameras/realistic.cpp
@@ -26,17 +26,18 @@ float RealisticCamera::GenerateRay(const CameraSample &sample, Ray *ray) const {
 
 RealisticCamera *CreateRealisticCamera(const ParamSet &params,
         const AnimatedTransform &cam2world, Film *film) {
-	// Extract common camera parameters from \use{ParamSet}
-	float hither = params.FindOneFloat("hither", -1);
-	float yon = params.FindOneFloat("yon", -1);
-	float shutteropen = params.FindOneFloat("shutteropen", -1);
-	float shutterclose = params.FindOneFloat("shutterclose", -1);
 
 	// Realistic camera-specific parameters
-	string specfile = params.FindOneString("specfile", "");
-	float filmdistance = params.FindOneFloat("filmdistance", 70.0); // about 70 mm default to film
- 	float fstop = params.FindOneFloat("aperture_diameter", 1.0);	
-	float filmdiag = params.FindOneFloat("filmdiag", 35.0);
+	string specfile		= params.FindOneString("specfile", "");
+	float filmdistance	= params.FindOneFloat("filmdistance", 70.0); // about 70 mm default to film
+ 	float fstop			= params.FindOneFloat("aperture_diameter", 1.0);	
+	float filmdiag		= params.FindOneFloat("filmdiag", 35.0);
+
+	// Extract common camera parameters from \use{ParamSet}
+	float hither		= params.FindOneFloat("hither", -1);
+	float yon			= params.FindOneFloat("yon", -1);
+	float shutteropen	= params.FindOneFloat("shutteropen", -1);
+	float shutterclose	= params.FindOneFloat("shutterclose", -1);
 
 	Assert(hither != -1 && yon != -1 && shutteropen != -1 &&
 		shutterclose != -1 && filmdistance!= -1);
